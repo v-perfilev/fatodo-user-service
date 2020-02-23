4963c47c2d656f7abc9314b32b84a80d84b28285
Vladimir Perfilev <persoff68@gmail.com>
2020-02-23 19:27:53 +0100

    add dataaccessexception handler

--- src/main/java/com/persoff68/fatodo/exception/translator/ExceptionTranslator.java
+++ src/main/java/com/persoff68/fatodo/exception/translator/ExceptionTranslator.java
@@ -1,15 +1,19 @@
 package com.persoff68.fatodo.exception.translator;
 
+import com.persoff68.fatodo.exception.constant.ExceptionTypes;
 import com.persoff68.fatodo.exception.util.ProblemUtils;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
@@ -44,4 +48,13 @@ class ExceptionTranslator implements ErrorController, ProblemHandling, SecurityA
         return new ResponseEntity<>(problem, entity.getHeaders(), entity.getStatusCode());
     }
 
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleMongoException(DataAccessException e, NativeWebRequest request) {
+        Problem problem = Problem.builder()
+                .withType(ExceptionTypes.DB_TYPE)
+                .withTitle(e.getMessage())
+                .withStatus(Status.BAD_REQUEST)
+                .build();
+        return create(e, problem, request);
+    }
 }
