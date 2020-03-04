8877a69a1d9f0658d467fa7c66a43cb35d2c714e
Vladimir Perfilev <persoff68@gmail.com>
2020-03-04 14:06:06 +0100

    add dirty context

--- src/test/java/com/persoff68/fatodo/ExtendedMongoSkeletonApplicationTests.java
+++ src/test/java/com/persoff68/fatodo/ExtendedMongoSkeletonApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@DirtiesContext
 class ExtendedMongoSkeletonApplicationTests {
 
     @Autowired
