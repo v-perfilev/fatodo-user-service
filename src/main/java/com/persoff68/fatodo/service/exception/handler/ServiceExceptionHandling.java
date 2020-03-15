70ade08e3105db44a66f7ef2467e20669d6d2793
Vladimir Perfilev <persoff68@gmail.com>
2020-03-15 19:00:46 +0100

    edit service exceptions

--- src/main/java/com/persoff68/fatodo/service/exception/handler/ServiceExceptionHandling.java
+++ src/main/java/com/persoff68/fatodo/service/exception/handler/ServiceExceptionHandling.java
@@ -1,7 +1,8 @@
 package com.persoff68.fatodo.service.exception.handler;
 
-import com.persoff68.fatodo.exception.handler.ExceptionTranslator;
-import com.persoff68.fatodo.service.exception.CommonDatabaseProblem;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.persoff68.fatodo.exception.attribute.AttributeHandler;
+import com.persoff68.fatodo.service.exception.CommonDatabaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -9,22 +10,22 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.zalando.problem.Problem;
-import org.zalando.problem.ThrowableProblem;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE + 1)
 @RequiredArgsConstructor
 public class ServiceExceptionHandling {
 
-    private final ExceptionTranslator exceptionTranslator;
+    private final ObjectMapper objectMapper;
 
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<Problem> handleDatabaseException(Exception e, HttpServletRequest request) {
-        ThrowableProblem problem = new CommonDatabaseProblem(e.getMessage());
-        return exceptionTranslator.process(problem, request.getRequestURI());
+    public ResponseEntity<String> handleDatabaseException(Exception e, HttpServletRequest request) throws IOException {
+        Exception exception = new CommonDatabaseException(e.getMessage());
+        AttributeHandler attributeHandler = new AttributeHandler(request, exception);
+        return attributeHandler.getResponseEntity(objectMapper);
     }
 
 }
