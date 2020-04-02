e6e4ff4593ac0bd24e97312f99798ae00b668c4d
Vladimir Perfilev <persoff68@gmail.com>
2020-04-02 16:14:23 +0200

    TDM-25 fix contracts

--- src/test/resources/contracts/auth/getByEmail.groovy
+++ src/test/resources/contracts/auth/getByEmail.groovy
@@ -7,13 +7,11 @@ Contract.make {
     description 'should return status 200 and UserPrincipalDTO'
     request {
         method GET()
-        url("/auth/email/test_local@email.com")
         url($(
-                consumer(regex('\\/auth\\/email\\/[\\w-\\@\\.]+')),
+                consumer(regex('\\/auth\\/email\\/[\\w-\\.]+(%40|@)[\\w-\\.]+')),
                 producer("/auth/email/test_local@email.com")
         ))
         headers {
-            contentType applicationJson()
             header 'Authorization': $(
                     consumer(containing("Bearer")),
                     producer("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXNlcm5hbWUiOiJ0ZXN0X3N5c3RlbSIsImF1dGhvcml0aWVzIjoiUk9MRV9TWVNURU0iLCJpYXQiOjAsImV4cCI6MzI1MDM2NzY0MDB9.EV6TMwQSB2XSTnQuB6LQbLETQmWEullfxSOmGDrlsdk93DDWfqr3VQGti6pMmmbUfgCyP9yyWjlWK50dYHYnEg")
--- src/test/resources/contracts/auth/getByUsername.groovy
+++ src/test/resources/contracts/auth/getByUsername.groovy
@@ -12,7 +12,6 @@ Contract.make {
                 producer("/auth/username/test_username_local")
         ))
         headers {
-            contentType applicationJson()
             header 'Authorization': $(
                     consumer(containing("Bearer")),
                     producer("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXNlcm5hbWUiOiJ0ZXN0X3N5c3RlbSIsImF1dGhvcml0aWVzIjoiUk9MRV9TWVNURU0iLCJpYXQiOjAsImV4cCI6MzI1MDM2NzY0MDB9.EV6TMwQSB2XSTnQuB6LQbLETQmWEullfxSOmGDrlsdk93DDWfqr3VQGti6pMmmbUfgCyP9yyWjlWK50dYHYnEg")
