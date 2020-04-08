b6b2dc432956e63fdd742abd7a4adc1b07f497fc
Vladimir Perfilev <persoff68@gmail.com>
2020-04-08 18:34:48 +0200

    TDM-34 change id type back to String

--- src/main/java/com/persoff68/fatodo/model/dto/AbstractAuditingDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/AbstractAuditingDTO.java
@@ -3,12 +3,11 @@ package com.persoff68.fatodo.model.dto;
 import lombok.Data;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Data
 public abstract class AbstractAuditingDTO {
 
-    private UUID id;
+    private String id;
     private String createdBy;
     private Instant createdDate;
     private String lastModifiedBy;
