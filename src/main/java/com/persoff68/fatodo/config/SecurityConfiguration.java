cc2b6d628f67bf5068f66efa884c547f1edbeb05
Vladimir Perfilev <persoff68@gmail.com>
2020-10-17 01:00:43 +0200

    TDM-70 fix bug with password encoder

--- src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import com.persoff68.fatodo.config.constant.AuthorityType;
 import com.persoff68.fatodo.security.filter.JwtTokenFilter;
 import com.persoff68.fatodo.security.filter.SecurityLocaleFilter;
 import com.persoff68.fatodo.security.filter.SecurityProblemSupport;
-import com.persoff68.fatodo.security.local.LocalPasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,7 +13,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -33,11 +31,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new LocalPasswordEncoder();
     }
 
     @Override
--- src/main/java/com/persoff68/fatodo/security/local/LocalPasswordEncoder.java
+++ src/main/java/com/persoff68/fatodo/security/local/LocalPasswordEncoder.java
@@ -1,7 +1,9 @@
 package com.persoff68.fatodo.security.local;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component("passwordEncoder")
 public class LocalPasswordEncoder extends BCryptPasswordEncoder {
 
     public LocalPasswordEncoder() {
