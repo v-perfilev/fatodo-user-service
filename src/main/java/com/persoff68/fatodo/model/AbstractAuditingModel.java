f8c0387eb2825bd4b30249b27f0b0bc080e362ab
Vladimir Perfilev <persoff68@gmail.com>
2020-10-26 00:23:41 +0100

    TDM-63 fix auditing bug

--- src/main/java/com/persoff68/fatodo/model/AbstractAuditingModel.java
+++ src/main/java/com/persoff68/fatodo/model/AbstractAuditingModel.java
@@ -6,13 +6,14 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public abstract class AbstractAuditingModel extends AbstractModel {
+public abstract class AbstractAuditingModel extends AbstractModel implements Persistable<UUID> {
 
     @CreatedBy
     protected UUID createdBy;
@@ -25,6 +26,11 @@ public abstract class AbstractAuditingModel extends AbstractModel {
 
     @LastModifiedDate
     protected Instant lastModifiedAt;
+
+    @Override
+    public boolean isNew() {
+        return createdBy == null && createdAt == null;
+    }
 
 }
 
