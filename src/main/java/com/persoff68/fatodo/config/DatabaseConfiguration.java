d1429528fc338c14bd211c26fde33b3e85a5bc84
Vladimir Perfilev <persoff68@gmail.com>
2022-04-29 19:00:35 +0200

    TDM-132 migrate to spring boot 2.6.7 and java 17

--- src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
@@ -1,11 +1,8 @@
 package com.persoff68.fatodo.config;
 
-import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
-import com.github.cloudyrock.spring.v5.MongockSpring5;
 import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -15,9 +12,9 @@ import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.index.IndexOperations;
 import org.springframework.data.mongodb.core.index.IndexResolver;
 import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
-import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -41,24 +38,11 @@ public class DatabaseConfiguration {
         return new LocalValidatorFactoryBean();
     }
 
-    @Bean
-    public MongockSpring5.MongockInitializingBeanRunner mongockInitializingBeanRunner(
-            ApplicationContext springContext,
-            MongoTemplate mongoTemplate) {
-        String scanPath = this.getClass().getPackageName() + ".database.migrations";
-        return MongockSpring5.builder()
-                .setDriver(SpringDataMongo3Driver.withDefaultLock(mongoTemplate))
-                .addChangeLogsScanPackage(scanPath)
-                .setSpringContext(springContext)
-                .buildInitializingBeanRunner();
-    }
-
     @EventListener(ApplicationReadyEvent.class)
     public void initIndicesAfterStartup() {
         Object mappingContext = this.mongoConverter.getMappingContext();
-        if (mappingContext instanceof MongoMappingContext) {
-            MongoMappingContext mongoMappingContext = (MongoMappingContext) mappingContext;
-            for (BasicMongoPersistentEntity<?> persistentEntity : mongoMappingContext.getPersistentEntities()) {
+        if (mappingContext instanceof MongoMappingContext mongoMappingContext) {
+            for (MongoPersistentEntity<?> persistentEntity : mongoMappingContext.getPersistentEntities()) {
                 Class<?> clazz = persistentEntity.getType();
                 if (clazz.isAnnotationPresent(Document.class)) {
                     IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
