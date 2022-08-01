2bcc7a5fe991eb13e1faa8d295fd46f90d3d88d3
Vladimir Perfilev <persoff68@gmail.com>
2022-08-01 12:30:05 +0200

    TDM-150 edit request mapping annotation uses

--- src/main/java/com/persoff68/fatodo/web/rest/UserDataController.java
+++ src/main/java/com/persoff68/fatodo/web/rest/UserDataController.java
@@ -25,41 +25,35 @@ public class UserDataController {
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @GetMapping(value = "/username")
+    @GetMapping("/username")
     public ResponseEntity<List<String>> getAllUsernamesByIds(@RequestParam("ids") List<UUID> idList) {
         List<User> userList = userService.getAllByIds(idList);
-        List<String> usernameList = userList.stream()
-                .map(User::getUsername)
-                .toList();
+        List<String> usernameList = userList.stream().map(User::getUsername).toList();
         return ResponseEntity.ok(usernameList);
     }
 
-    @GetMapping(value = "/ids/{username}/username-part")
+    @GetMapping("/ids/{username}/username-part")
     public ResponseEntity<List<UUID>> getAllIdsByUsernamePart(@PathVariable @NotBlank String username) {
         List<User> userList = userService.getAllByUsernamePart(username);
-        List<UUID> userSummaryDTOList = userList.stream()
-                .map(User::getId)
-                .toList();
+        List<UUID> userSummaryDTOList = userList.stream().map(User::getId).toList();
         return ResponseEntity.ok(userSummaryDTOList);
     }
 
-    @GetMapping(value = "/summary/{usernamePart}/username-part")
+    @GetMapping("/summary/{usernamePart}/username-part")
     public ResponseEntity<List<UserSummaryDTO>> getAllByUsernamePart(@PathVariable @NotBlank String usernamePart) {
         List<User> userList = userService.getAllByUsernamePart(usernamePart);
-        List<UserSummaryDTO> userSummaryDTOList = userList.stream()
-                .map(userMapper::pojoToSummaryDTO)
-                .toList();
+        List<UserSummaryDTO> userSummaryDTOList = userList.stream().map(userMapper::pojoToSummaryDTO).toList();
         return ResponseEntity.ok(userSummaryDTOList);
     }
 
-    @GetMapping(value = "/summary/{username}/username")
+    @GetMapping("/summary/{username}/username")
     public ResponseEntity<UserSummaryDTO> getByUsername(@PathVariable @NotBlank String username) {
         User user = userService.getByUsername(username);
         UserSummaryDTO userSummaryDTO = userMapper.pojoToSummaryDTO(user);
         return ResponseEntity.ok(userSummaryDTO);
     }
 
-    @GetMapping(value = "/summary/{usernameOrEmail}/username-or-email")
+    @GetMapping("/summary/{usernameOrEmail}/username-or-email")
     public ResponseEntity<UserSummaryDTO> getByUsernameOrEmail(@PathVariable @NotBlank String usernameOrEmail) {
         User user = userService.getByUsernameOrEmail(usernameOrEmail);
         UserSummaryDTO userSummaryDTO = userMapper.pojoToSummaryDTO(user);
